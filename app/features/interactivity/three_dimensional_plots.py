b122bc779c224f608f7256f7c22221e2e91f9911
Erik Buther <butcer0@gmail.com>
2020-03-16 11:29:19 -0400

    Introduced Interactivity and Processing Essential Recipes
    Introduced Interactivity and Processing Essential Recipes

---
+++ app/features/interactivity/three_dimensional_plots.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+# generate example data
+import numpy as np
+x,y = np.meshgrid(np.linspace(-1,1,115),np.linspace(-1,1,115))
+z = np.cos(x*np.pi)*np.sin(y*np.pi)
+
+# actual plotting example
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+# rstride and cstride are row and column stride (step size)
+ax.plot_surface(x,y,z,rstride=1,cstride=1,cmap='hot')
+ax.set_xlabel(r'$x$')
+ax.set_ylabel(r'$y$')
+ax.set_zlabel(r'$\cos(\pi x) \sin(\pi y)$')
+plt.show()
